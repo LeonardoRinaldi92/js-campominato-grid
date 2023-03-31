3ccf298472a275e37a225a6bdd68ecc1814bfa8d
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-31 15:17:51 +0200

    numero random aggiunto solo al 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 let grid = document.querySelector(".griglia")
+let numeri= [];
+
+for (i=1;i<=100;i++){
+    numeroRandom = (Math.floor(Math.random() * 100))+1;
+    if (numeri.includes(numeroRandom) ) {
+        i--
+    } else {
+        numeri.push(numeroRandom)
+    }
+}
+
 
 function creaBox (classeScelta,numero) {
     let elemento = document.createElement( "div" )
@@ -12,6 +23,6 @@ function creaBox (classeScelta,numero) {
 
 }
 
- for (let i= 1; i<=100; i++){
-      creaBox("box",i)
+ for (let i= 0; i<=99; i++){
+      creaBox("box",numeri[i])
   }
