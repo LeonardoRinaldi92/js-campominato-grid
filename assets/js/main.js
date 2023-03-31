2d1687757a447d3db5d43a4995adbd53d5a764cb
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-31 16:45:41 +0200

    completo con bonus (no grafica...forse domenica)

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,10 +18,17 @@ function numericasuali (int){
 
 
 //funziona crea un box  
-function creaBox (classeScelta,numero) {
+function creaBox (classeScelta,numero,difficolta) {
     let elemento = document.createElement( "div" )
     elemento.className = classeScelta
     elemento.innerText = numero
+     if (difficolta === 100 ){
+         elemento.classList.add("sq10")
+    } else if (difficolta === 81) {
+        elemento.classList.add("sq9")
+    } else if (difficolta === 49) {
+        elemento.classList.add ("sq7")
+    }
     elemento.addEventListener("click",function(){
         this.classList.add("blu");
         console.log(numero)
@@ -33,9 +40,20 @@ function creaBox (classeScelta,numero) {
 //al cambio delle opzioni sulla select
 difficolta.addEventListener("change", function(){
     difficolta = parseInt(document.querySelector("#difficolta").value)
+    grid.innerHTML = "";
+    if  (difficolta === 100 ){
+        grid.classList.remove("w72", "w56");
+        grid.classList.add("w80", "flex")
+   } else if (difficolta === 81) {
+    grid.classList.remove("80", "w56");
+       grid.classList.add("w72", "flex")
+   } else if (difficolta === 49) {
+    grid.classList.remove("w72", "w80");
+       grid.classList.add ("w56", "flex")
+   }
     let numeri = numericasuali(difficolta);
 
     for (let i= 1; i<=numeri.length; i++){
-        creaBox("box",numeri[i-1])
+        creaBox("box",numeri[i-1],difficolta)
   }
 })    
