b9730ae7177eba2f62add97e1ad6e36890f25fd0
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-03-31 14:54:59 +0200

    struttura colori e ciclo for

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,17 @@
 let grid = document.querySelector(".griglia")
-console.log(grid)
 
 function creaBox (classeScelta,numero) {
     let elemento = document.createElement( "div" )
     elemento.className = classeScelta
     elemento.innerText = numero
+    elemento.addEventListener("click",function(){
+        this.classList.add("blu");
+        console.log(numero)
+    })
     return grid.append(elemento)
 
 }
 
-creaBox("box",99)
-
-
-
-// let quadratini =
-// for (let i= 1; i<=100; i++){
-//     griglia.
-// }
+ for (let i= 1; i<=100; i++){
+      creaBox("box",i)
+  }
